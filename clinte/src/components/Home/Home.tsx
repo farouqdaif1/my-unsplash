802fb1c046a01b72175f4b448d3d37fcb16db293
farouqdaif1 <daiffarouq@gmail.com>
2023-07-21 17:03:52 +0300

    Add Setup for front-end

--- clinte/src/components/Home/Home.tsx
+++ clinte/src/components/Home/Home.tsx
@@ -1,8 +1,18 @@
 import Item from "./Item/Item"
 const Home = () => {
+  const data: [{ label: string; link: string; }] =
+    [{
+      label: "Helllo Farouq"
+      , link: "https://media.istockphoto.com/id/640318118/photo/sunset-over-indian-ocean.jpg?s=612x612&w=0&k=20&c=DLKuEcWW9YwwNid-3ypLu2AJ4uPC4aXoIzyFFjDHUmM="
+    }]
   return (
-    <div>
-      <Item/>
+    <div className="home">
+      {
+        data.map((ele :{ label: string; link: string; },index: number) => 
+          <Item key={index} ele={ele} />
+        )
+    }
+      
     </div>
   )
 }
--- clinte/src/components/Home/Item/Item.tsx
+++ clinte/src/components/Home/Item/Item.tsx
@@ -1,8 +1,18 @@
+interface childeProps {
+  ele: {
+    label: string;
+    link: string;
+}
+}
 
-const Item = () => {
+const Item = ({ele }:childeProps) => {
   return (
-    <div>
-      Item
+    <div className="image-item">
+      <img src={ele.link} alt={ele.label} />
+      <div className="hover">
+        <button className="delete">Delete</button>
+        <h2 className="title">{ele.label}</h2>
+      </div>
     </div>
   )
 }
