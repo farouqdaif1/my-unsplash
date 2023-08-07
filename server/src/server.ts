67cc9c44ddd6309502301ff0e9d7b5d47b19249c
farouqdaif1 <daiffarouq@gmail.com>
2023-08-07 15:22:24 +0300

    Setup mongoose and Envalid

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
-import express from "express";
-const app = express();
-const Port = 5000;
-app.get('/', (req, res) => {
-    res.send("Hello, World!")
-})
-app.listen(Port, () =>{
-    console.log("Server running on port: " + Port)
-})
+import app from "./app";
+import env from "./util/validateEnv"
+import mongoose from 'mongoose';
+const PORT = env.PORT
+mongoose.connect(env.MONGO_CONNECTION_STRING).then(() => {
+    console.log("Mongoose Connected")
+    app.listen(PORT, () => {
+        console.log(`Server Running on Port: http://localhost:${PORT}`)
+    })
+}).catch((error) => console.log(`${error} did not connect`));
