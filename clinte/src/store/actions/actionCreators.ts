f6c59692895cedaa196cc6bf904f92d06e900e95
farouqdaif1 <daiffarouq@gmail.com>
2023-08-09 18:32:29 +0300

    Add structure file for redux

--- clinte/src/store/actions/actionCreators.ts
+++ clinte/src/store/actions/actionCreators.ts
@@ -1,16 +1,16 @@
 import * as api from '../api/index.js';
-import { FETCH_ALL, ADD_IMAGE, REMOVE_IMAGE } from "../constants/actionTypes";
+import { FETCH_ALL, ADD_IMAGE, REMOVE_IMAGE } from "../constants/actionTypes.js";
 //Action creators
 
-export const getImage= () => async (dispatch: DispatchType) => {
+export const getImage = () => async (dispatch: DispatchType) => {
     try {
-        const { data  } = await api.fetchImages();
+        const { data } = await api.fetchImages();
         dispatch({ type: FETCH_ALL, payload: data })
     } catch (error) {
         console.log(error);
     }
 }
-export const addImage= (image: ImageData) => async (dispatch: DispatchType) => {
+export const addImage = (image: ImageData) => async (dispatch: DispatchType) => {
     try {
         const { data } = await api.createImage(image);
         dispatch({ type: ADD_IMAGE, payload: data })
--- clinte/src/store/reducers/reducer.ts
+++ clinte/src/store/reducers/reducer.ts
@@ -1,8 +1,8 @@
 import { FETCH_ALL, ADD_IMAGE, REMOVE_IMAGE } from '../constants/actionTypes';
-export default (images:ImageState=[] , action: ImageAction) : ImageState => {
+export default (images: ImageState = [], action: ImageAction): ImageState => {
     switch (action.type) {
         case REMOVE_IMAGE:
-            return images.filter((image)=> image.id !== action.payload.id);
+            return images.filter((image) => image.id !== action.payload.id);
         case FETCH_ALL:
             return action.payload;
         case ADD_IMAGE:
