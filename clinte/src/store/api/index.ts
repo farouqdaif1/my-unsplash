8b3af42e1e0aac6ffeb78bfa00dee9c5c084523e
Farouq Daif <daiffarouq@gmail.com>
2023-09-07 20:08:25 +0300

    Update index.ts

--- clinte/src/store/api/index.ts
+++ clinte/src/store/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/images";
+const API_URL = "https://my-splash-server.onrender.com/api/images";
 export const fetchImages = () => axios.get<Image[]>(API_URL);
 export const searchImages = (searchTerm:string) => axios.get<Image[]>(`${API_URL}/search/${searchTerm}`);
 export const deleteImage = (id: string) => axios.delete(`${API_URL}/${id}`);
